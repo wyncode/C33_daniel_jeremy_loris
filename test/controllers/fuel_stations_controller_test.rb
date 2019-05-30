014241f6ae36e3a22b17293e51078898ba0e40d9
Andy Weiss <andyweiss1982@gmail.com>
2019-05-30 10:50:37 -0400

    Delete fuel_stations_controller_test.rb

--- test/controllers/fuel_stations_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class FuelStationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get fuel_stations_index_url
-    assert_response :success
-  end
-
-end
