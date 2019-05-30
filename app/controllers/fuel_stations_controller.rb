9a86fe31cac9842ca8866adb26f3741b230a16e1
Jeremy <jeremyamartins@gmail.com>
2019-05-29 20:50:37 -0400

    Made a table, and paginated with a will_paginate gem

--- app/controllers/fuel_stations_controller.rb
+++ app/controllers/fuel_stations_controller.rb
@@ -1,15 +1,15 @@
+
+
 class FuelStationsController < ApplicationController
   
   def index 
-    
-    if !params[:origin] || !params[:destination]
-      @stations = []
+    if params[:origin].present? && params[:destination].present?
+      @stations = AlternateFuelStationFinder.new(params[:origin], params[:destination]).run rescue []
+      if @stations.count >= 5
+        @stations = @stations.paginate(page: params[:page], per_page: 5)
+      end
     else 
-      @stations = AlternateFuelStationFinder.new(params[:origin], params[:destination]).run
+      @stations = []
     end
-
-    
-  end #def
-
-
+  end
 end
