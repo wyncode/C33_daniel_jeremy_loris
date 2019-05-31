967bf84fa556cd2e4046b0c3d953a8b33a2387a0
Daniel McGlinchy <danielmcglinchy01@gmail.com>
2019-05-30 20:49:57 -0400

    created searchable api of db

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,6 +1,14 @@
 class VehiclesController < ApplicationController
+ 
+
   def index
     vehicles = Vehicle.all
+    vehicles = vehicles.filter_model(params[:model]) if params[:model]
+    vehicles = vehicles.filter_make(params[:make]) if params[:make]
     render json: vehicles
+  end
+
+  def show
+   
   end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,4 @@
 class Vehicle < ApplicationRecord
+    scope :filter_make, -> (search_make) { where(make: search_make ) }
+    scope :filter_model, -> (search_model) { where(name:search_model ) }
 end
