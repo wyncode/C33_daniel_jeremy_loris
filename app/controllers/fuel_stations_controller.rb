708574aae824070a3a4ac75d092ffe5d47ad05cf
Jeremy <jeremyamartins@gmail.com>
2019-06-01 18:04:59 -0400

    added code to the fuel stations controller

--- app/controllers/fuel_stations_controller.rb
+++ app/controllers/fuel_stations_controller.rb
@@ -15,5 +15,28 @@ class FuelStationsController < ApplicationController
     page      = (params[:page] || 1).to_i
     per_page  = 50
     @stations = @stations.paginate(page: page, per_page: per_page)
+  
+    respond_to do |format|
+      format.html
+      format.json do
+        @stations = AlternateFuelStationFinder.all
+        render json:  {
+                        type: "FeatureCollection",
+                        features: @stations.map do |station|
+                          {
+                            type: "Feature",
+                            geometry: {
+                              type: "Point",
+                              coordinates: [station.longitude, station.latitude]
+                            },
+                            properties: {
+                              name: station.name,
+                              id: station.id
+                            }
+                          }
+                        end
+                      }
+      end
+    end
   end
 end
